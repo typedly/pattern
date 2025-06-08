849c1bff33388a6fdee698669ae8606d7dc9b95a
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-06-08 09:20:22 +0000

    docs(`PatternFromFlags`): update.

--- src/lib/type/pattern-from-flags.type.ts
+++ src/lib/type/pattern-from-flags.type.ts
@@ -1,3 +1,11 @@
+/**
+ * @description Represents the pattern flags for validation.
+ * @export
+ * @template {boolean} Lowercase The lowercase setting for the pattern validation.
+ * @template {boolean} Uppercase The uppercase setting for the pattern validation.
+ * @template {boolean} Digit The digit setting for the pattern validation.
+ * @template {boolean} Special The special character setting for the pattern validation.
+ */
 export type PatternFromFlags<
   Lowercase extends boolean,
   Uppercase extends boolean,
